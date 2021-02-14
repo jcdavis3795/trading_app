5a857f41fd7f1c827098c6efabae69d5c1ccee35
Jordan <46761218+jcdavis3795@users.noreply.github.com>
2021-02-14 12:59:30 -0600

    Delete trading_app directory

--- trading_app/credentials.py
+++
@@ -1,6 +0,0 @@
-API_KEY = ""
-API_SECRET_KEY = ""
-
-ENDPOINT_URL = "https://paper-api.alpaca.markets"
-ACCOUNT_URL = "{}/v2/account".format(ENDPOINT_URL)
-ORDERS_URL = "{}/v2/orders".format(ENDPOINT_URL)
